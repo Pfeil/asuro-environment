0416ebd690357c39f5208b19012d7dc7710934e4
Andreas Pfeil <verpfeilt@pfmb.fritz.box>
2015-06-13 21:12:56 +0200

    Improved light searching abilities.
    I still need to implement reactions on dampers and refactor the code for
    modular usage.

--- examples/6_ReactiveBehaviour/app.c
+++ examples/6_ReactiveBehaviour/app.c
@@ -19,45 +19,120 @@
  *   any later version.                                                    *
  ***************************************************************************/
 #include <avr/interrupt.h> //ISR(), cli(), sei()
+#include <stdlib.h>
 
 #include "asuro.h" // Init(), NULL, Motor*(), ...
 #include "TimerService.h" // ts_init(), ts_addFunction()
 
-unsigned int data[] = {0,0};
-void fun1(void) {
-	LineData(data);
-	//print
-	PrintInt(data[0]);
+#define MIN_SPEED 70
+#define MAX_SPEED 150
+#define LIGHT_FAULT 85
+//#define GRADIENT 0.31875
+
+unsigned int light[2];
+unsigned char dampers;
+// output variables
+unsigned char speed[2];
+unsigned char direction[2];
+
+// for LEFT line data
+inline int line_correction(int linedata) {
+	float x = linedata / 1024.0;
+	return linedata + (LIGHT_FAULT * 4 * x * (x-1));
+}
+
+inline int lightToSpeed(int light)
+{
+	float x = light / 1024.0;
+	return MIN_SPEED + (MAX_SPEED - MIN_SPEED) * (1-x);
+}
+
+void handle_light(void)
+{
+	light[LEFT] = line_correction(light[LEFT]);
+	if (light[LEFT] > light[RIGHT]) {
+		MotorDir(FWD,FWD);
+		speed[RIGHT] = lightToSpeed(light[RIGHT]);
+		speed[LEFT] = MIN_SPEED;
+		//BackLED(ON,OFF);
+	} else {
+		MotorDir(FWD,FWD);
+		speed[LEFT] = lightToSpeed(light[LEFT]);
+		speed[RIGHT] = MIN_SPEED;
+		//BackLED(OFF,ON);
+	}
+}
+
+void fetchData(void) {
+	dampers = 0; //just trying
+	dampers  = PollSwitch() & 0b00111111;
+	Msleep(5);
+	dampers &= PollSwitch() & 0b00111111;
+	Msleep(5);
+	dampers &= PollSwitch() & 0b00111111;
+	Msleep(5);
+	dampers &= PollSwitch() & 0b00111111;
+	LineData(light);
+}
+
+//void handle_dampers(void) {
+//	int outer = 
+//	if (dampers >= 0b00100001 || dampers & 0b00010010) {
+//		
+//	}
+//	Go(-100, 90);
+//	Turn(90, 90);
+//	Msleep(10);
+//}
+
+void decide(void) {
+	//if (dampers) { handle_dampers(); }
+	handle_light();
+}
+
+void act(void) {
+	MotorDir(FWD,FWD);
+	MotorSpeed(speed[LEFT], speed[RIGHT]);
+}
+
+void debugLineData(void) {
+	unsigned short delay = 5;
+	SerPrint("a,");
+	Msleep(delay);
+	PrintInt(light[LEFT]);
+	Msleep(delay);
+	SerPrint(",");
+	Msleep(delay);
+	PrintInt(light[RIGHT]);
+	Msleep(delay);
+	SerPrint("\n");
+}
+
+
+unsigned int ododata[2];
+
+void debugOdoData(void) {
+	OdometryData(ododata);
+	PrintInt(ododata[0]);
 	SerPrint(";");
-	PrintInt(data[1]);
+	PrintInt(ododata[1]);
 	SerPrint("\n\r");
 }
 
 int main (void)
 {
 	Init();
-	ts_init();
-	
-	FrontLED(ON);
-	void (*pfun1) (void) = &fun1;
-	ts_addFunction(pfun1,100);
-	
-	//GoTurn(1000,0,100);
-	unsigned char t1, t2, LEDl, LEDr;
+	EncoderInit();
+	//FrontLED(ON);
+	//MotorDir(FWD,FWD);
+	//MotorSpeed(80,80);
 	while(1)
 	{
-		t1 = PollSwitch();
-		t2 = PollSwitch();
-		t1 = PollSwitch();
-		
-		if (t1 == 0 && t2 == 0) {
-			BackLED(OFF,OFF);
-		} else if (t1 && t2 && t1 == t2) {
-			if (t1 == 0x01) {
-				BackLED(ON,OFF);
-			} else if (t1 == 0x20) {
-				BackLED(OFF,ON);
-			}
-		}
+		fetchData();
+		//debugOdoData();
+		//debugLineData();
+		decide();
+		act();
+		//Msleep(5);
 	}
 }
